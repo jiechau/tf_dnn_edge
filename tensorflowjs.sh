f179c222db4247e8641c2329bf29f66e68693609
jiechau <jiechau@gmail.com>
2024-06-06 17:18:56 +0800

    update tfjs

--- tensorflowjs.sh
+++ tensorflowjs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-tensorflowjs_converter --input_format=keras  save/tf_dnn.h5 tfjs/ # for .h5
 tensorflowjs_converter save_all tfjs/ # for model.export('save_all')
 tensorflowjs_converter --input_format=tf_saved_model  save_all tfjs/ # for model.export('save_all')
 tensorflowjs_converter --input_format=tf_saved_model --output_format=tfjs_graph_model save_tf tfjs # doesn't work
+#
+tensorflowjs_converter --input_format=keras  save/tf_dnn.h5 tfjs/ # for .h5
 tensorflowjs_converter --input_format=keras_keras --output_format=tfjs_layers_model save/tf_dnn.keras tfjs/
